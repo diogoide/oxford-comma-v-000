9f2a86b045e4402878515b62dd71f8e782af279d
Diogo Ide <diogoide@gmail.com>
2018-04-01 20:11:12 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
   elsif array.size == 3
     missing_last_word = array.pop
     missing_last_word << "and"
-    missing_last_word << "array.last"
+    missing_last_word << array.last
     missing_last_word.join(", ")
   else
     missing_last_word = array.pop
