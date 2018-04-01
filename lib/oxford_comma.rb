9551e7e0a9ba814cfaa21d18f1d78715cea1e407
Diogo Ide <diogoide@gmail.com>
2018-04-01 20:45:14 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,13 +3,14 @@ def oxford_comma(array)
     array.join
   elsif array.size < 3
     array.join(" and ")
-  elsif array.size == 3
+  else
+    array[0..-2].join(", ") << ", and " << array[-1]
     #missing_last_word = []
     #missing_last_word = array.pop
     #array << "and"
     #array << missing_last_word
-    array.insert(2, "and")
-    array.join(", ")
+    #array.insert(2, "and")
+    #array.join(", ")
   #else
     #missing_last_word = []
     #missing_last_word = array.pop
