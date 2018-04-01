070157a14ffd31c108c45ec345a7a8cba58541b3
Diogo Ide <diogoide@gmail.com>
2018-04-01 19:55:11 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,12 +6,12 @@ def oxford_comma(array)
   elsif array.size == 3
     missing_last_word = array.pop
     missing_last_word << "and"
-    missing_last_word << array.last
+    missing_last_word << "array.last"
     missing_last_word.join(", ")
   else
     missing_last_word = array.pop
     missing_last_word << "and"
-    missing_last_word << array.last
+    missing_last_word << "array.last"
     missing_last_word.sort = final_array
     final_array.join(", ")
   end
